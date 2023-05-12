97b5711462df2bab216cb2690da5864e2fba618f
IamJaehyun <lvet0330@gmail.com>
2023-05-12 15:00:41 +0900

    #1 <back> 레포지토리 생성

---
+++ src/main/java/com/cal/casealotbackend/CasealotBackendApplication.java
@@ -0,0 +1,13 @@
+package com.cal.casealotbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CasealotBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CasealotBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cal/casealotbackend/CasealotBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cal.casealotbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CasealotBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
