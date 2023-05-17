3e772f7e33391a7b4851d345f9670becef232f50
IamJaehyun <lvet0330@gmail.com>
2023-05-17 23:03:20 +0900

    fix: BaseEntity 적용 및 어노테이션 수정
        @CreatedDate
        @Column(nullable = false, updatable = false, columnDefinition = "TIMESTAMP DEFAULT CURRNET_TIMESTAMP") 자동으로 들어가도록

--- src/main/java/com/cal/casealotbackend/CasealotBackendApplication.java
+++ src/main/java/com/cal/casealotbackend/CasealotBackendApplication.java
@@ -2,8 +2,10 @@ package com.cal.casealotbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class CasealotBackendApplication {
 
     public static void main(String[] args) {
