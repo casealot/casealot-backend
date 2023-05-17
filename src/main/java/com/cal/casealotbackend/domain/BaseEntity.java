aeec51f16242804d8123a6e688996b8160eb9622
IamJaehyun <lvet0330@gmail.com>
2023-05-17 23:03:20 +0900

    fix: BaseEntity 적용 및 어노테이션 수정
        @CreatedDate
        @Column(nullable = false, updatable = false, columnDefinition = "TIMESTAMP DEFAULT CURRNET_TIMESTAMP") 자동으로 들어가도록

--- src/main/java/com/cal/casealotbackend/domain/BaseEntity.java
+++ src/main/java/com/cal/casealotbackend/domain/BaseEntity.java
@@ -3,23 +3,26 @@ package com.cal.casealotbackend.domain;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @MappedSuperclass
-@Getter
-@Setter
-@ToString
+@EntityListeners(AuditingEntityListener.class)
 public abstract class BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false, updatable = false)
+    @CreatedDate
+    @Column(nullable = false, updatable = false, columnDefinition = "TIMESTAMP DEFAULT CURRNET_TIMESTAMP")
     private LocalDateTime createdAt;
 
+    @LastModifiedDate
     @Column(nullable = false)
     private LocalDateTime updatedAt;
 
