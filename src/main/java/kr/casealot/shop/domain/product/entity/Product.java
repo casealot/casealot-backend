1742008c297f3df8ecd5682d8518e365b1901383
ChangheeKim <kch0348@gmail.com>
2023-05-23 00:28:29 +0900

    feat: add product inquire, paging 1. 상품 조회, 페이징 처리

--- src/main/java/kr/casealot/shop/domain/product/entity/Product.java
+++ src/main/java/kr/casealot/shop/domain/product/entity/Product.java
@@ -1,9 +1,8 @@
 package kr.casealot.shop.domain.product.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import kr.casealot.shop.global.entity.BaseTimeEntity;
+import lombok.*;
+import net.minidev.json.annotate.JsonIgnore;
 
 import javax.persistence.*;
 
@@ -16,11 +15,15 @@ import javax.persistence.*;
 @AllArgsConstructor
 @Entity
 @Table(name = "PRODUCT")
-public class Product {
+public class Product extends BaseTimeEntity {
     @Id
     @GeneratedValue
     @Column(name = "PRODUCT_ID")
     private Long id;
+
+    /**
+     * TODO : review 조인
+     */
 
     @Column(name = "PRODUCT_NAME", length = 1024)
     private String name;
@@ -28,7 +31,43 @@ public class Product {
     @Column(name = "PRODUCT_CONTENT", length = 4000)
     private String content;
 
+    @Column(name = "PRODUCT_IMG_B", length = 512)
+    private String img_B;
+
+    @Column(name = "PRODUCT_IMG_M", length = 512)
+    private String img_M;
+
+    @Column(name = "PRODUCT_IMG_S", length = 512)
+    private String img_S;
+
+    @Column(name = "PRODUCT_PRICE")
+    private int price;
+    @Column(name = "PRODUCT_SALE")
+    private int sale;
+    @Column(name = "PRODUCT_VIEWS")
+    private int views;
+    @Column(name = "PRODUCT_SELLS")
+    private int sells;
     @Column(name = "PRODUCT_COLOR", length = 50)
     private String color;
+    @Column(name = "PRODUCT_SEASON", length = 50)
+    private String season;
+    /**
+     * NEW
+     * BEST ..etc
+     */
+    @Column(name = "PRODUCT_TYPE", length = 50)
+    private String type;
+
+    @Builder
+    public Product(String name, String content, int price, int views, int sells, String color, String season) {
+        this.name = name;
+        this.content = content;
+        this.price = price;
+        this.views = views;
+        this.sells = sells;
+        this.color = color;
+        this.season = season;
+    }
 
 }
--- src/main/java/kr/casealot/shop/domain/product/repository/ProductRepository.java
+++ src/main/java/kr/casealot/shop/domain/product/repository/ProductRepository.java
@@ -1,11 +1,13 @@
 package kr.casealot.shop.domain.product.repository;
 
 import kr.casealot.shop.domain.product.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
-    Product findById(String id);
-    Product deleteById(String id);
+    Page<Product> findAll(Pageable pageable);
 }
