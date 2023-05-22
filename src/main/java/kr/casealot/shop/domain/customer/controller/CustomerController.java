48565ad38383decb2da7f1a94e10838a33d1fa66
IamJaehyun <lvet0330@gmail.com>
2023-05-22 23:30:43 +0900

    feat: login success
    토큰 처리 해야함.

--- src/main/java/kr/casealot/shop/domain/customer/controller/CustomerController.java
+++ src/main/java/kr/casealot/shop/domain/customer/controller/CustomerController.java
@@ -4,6 +4,7 @@ import kr.casealot.shop.domain.customer.dto.CustomerDto;
 import kr.casealot.shop.domain.customer.entity.Customer;
 import kr.casealot.shop.domain.customer.repository.CustomerRepository;
 import kr.casealot.shop.domain.customer.service.CustomerService;
+import kr.casealot.shop.global.oauth.token.AuthToken;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
@@ -13,11 +14,15 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 @RequestMapping("/cal/v1/customer")
 public class CustomerController {
-    private final CustomerRepository customerRepository;
     private final CustomerService customerService;
 
     @PostMapping("/join")
     public Long join(@RequestBody CustomerDto customerDto){
         return customerService.join(customerDto);
+    }
+
+    @PostMapping("/login")
+    public AuthToken login(@RequestBody CustomerDto customerDto){
+        return customerService.login(customerDto);
     }
 }
