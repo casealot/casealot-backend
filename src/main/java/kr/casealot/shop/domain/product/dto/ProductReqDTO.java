a5bcf9512d1d2db73a3f8d87f59c8ea653d12225
ChangheeKim <kch0348@gmail.com>
2023-05-28 16:55:16 +0900

    feat: add sort query search 1. 상품 명 검색, 정렬 추가

--- src/main/java/kr/casealot/shop/domain/product/dto/ProductReqDTO.java
+++ src/main/java/kr/casealot/shop/domain/product/dto/ProductReqDTO.java
@@ -2,6 +2,10 @@ package kr.casealot.shop.domain.product.dto;
 
 import lombok.Data;
 import lombok.Getter;
+import org.springframework.core.annotation.Order;
+import org.springframework.data.domain.Sort;
+
+import java.util.List;
 
 @Data
 @Getter
@@ -10,6 +14,6 @@ public class ProductReqDTO {
     private int size;
     private int page;
 
-    private SortDTO sort;
+    private List<SortDTO> sort;
 
 }
