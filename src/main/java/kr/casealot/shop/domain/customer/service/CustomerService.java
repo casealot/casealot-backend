7796429c8520bb058d045eb7d130a62ddfcfdd26
IamJaehyun <lvet0330@gmail.com>
2023-05-28 15:03:32 +0900

    feat: parsing token, quit customer
    토큰 파싱해서 userId 얻어낸 다음 삭제 기능 구현

--- src/main/java/kr/casealot/shop/domain/customer/service/CustomerService.java
+++ src/main/java/kr/casealot/shop/domain/customer/service/CustomerService.java
@@ -1,15 +1,19 @@
 package kr.casealot.shop.domain.customer.service;
 
+import io.jsonwebtoken.Claims;
 import kr.casealot.shop.domain.customer.dto.CustomerDto;
 import kr.casealot.shop.domain.customer.entity.Customer;
 import kr.casealot.shop.domain.customer.repository.CustomerRepository;
 import kr.casealot.shop.global.oauth.entity.RoleType;
 import kr.casealot.shop.global.oauth.token.AuthToken;
 import kr.casealot.shop.global.oauth.token.AuthTokenProvider;
+import kr.casealot.shop.global.util.HeaderUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.Date;
 
 @Service
@@ -53,21 +57,23 @@ public class CustomerService {
         Date expiry = new Date(System.currentTimeMillis() + 3600000);
 
         // 토큰 생성
-        AuthToken token = authTokenProvider.createAuthToken(customer.getId(), expiry);
+        AuthToken token = authTokenProvider.createAuthToken(customer.getId(), RoleType.USER.getCode(), expiry);
 
         return token;
     }
 
-    public boolean deleteCustomer(String token) {
-        //TODO: 토큰으로 user의 Id를 알아내어 Customer 정보를 삭제해야 한다
+    @Transactional
+    public long deleteCustomer(HttpServletRequest request) {
+        String token = HeaderUtil.getAccessToken(request);
+
         AuthToken authToken = authTokenProvider.convertAuthToken(token);
-        // 토큰에서 사용자 아이디 추출
-        String userId = authToken.getTokenClaims().getSubject();
 
-        // 아이디로 사용자 삭제
-        boolean deleted = customerRepository.deleteById(userId);
+        Claims claims = authToken.getTokenClaims();
+        String userId = claims.getSubject();
 
-        return deleted;
+        System.out.println("ID: " + userId);
+
+        return customerRepository.deleteById(userId);
     }
 
 
