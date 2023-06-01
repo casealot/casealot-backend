446d5c5b248e51f400ba415f87cadee6b4deb01c
JaeYooooon <dhy04050@naver.com>
2023-06-01 19:16:28 +0900

    fix: 상품 등록, 수정, 삭제
    1. 유저 ID 받아옴
    2. 관리자만 등록, 수정, 삭제 가능

--- src/main/java/kr/casealot/shop/domain/product/dto/ProductDTO.java
+++ src/main/java/kr/casealot/shop/domain/product/dto/ProductDTO.java
@@ -1,13 +1,13 @@
 package kr.casealot.shop.domain.product.dto;
 
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import javax.persistence.Column;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class ProductDTO {
     private Long id;
     private Long userId;
