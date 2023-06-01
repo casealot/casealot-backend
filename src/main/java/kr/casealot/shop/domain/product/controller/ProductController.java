8c6657b58e19c8ac08735538b230e0ef17b5150d
JaeYooooon <dhy04050@naver.com>
2023-06-01 18:38:39 +0900

    feat: 상품 등록, 수정, 삭제
    1. productDTO 작성
    2. customer id 받아와야함
    3. 관리자 기능이라 admin 으로 빼야함

--- src/main/java/kr/casealot/shop/domain/product/controller/ProductController.java
+++ src/main/java/kr/casealot/shop/domain/product/controller/ProductController.java
@@ -1,14 +1,19 @@
 package kr.casealot.shop.domain.product.controller;
 
+import kr.casealot.shop.domain.product.dto.ProductDTO;
 import kr.casealot.shop.domain.product.dto.ProductResDTO;
 import kr.casealot.shop.domain.product.dto.ProductReqDTO;
 import kr.casealot.shop.domain.product.entity.Product;
 import kr.casealot.shop.domain.product.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.coyote.Response;
+import org.springframework.data.crossstore.ChangeSetPersister;
 import org.springframework.data.domain.*;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import static org.springframework.data.crossstore.ChangeSetPersister.*;
 
 @RestController
 @Slf4j
@@ -17,6 +22,33 @@ import org.springframework.web.bind.annotation.*;
 public class ProductController {
 
     private final ProductService productService;
+
+    @PostMapping("/admin/product")
+    public ResponseEntity<String> createProduct(@RequestBody ProductDTO productDTO){
+        productService.createProduct(productDTO);
+        return ResponseEntity.ok("create product");
+    }
+
+    @PutMapping("/admin/product/{product_id}")
+    public ResponseEntity<String> updateProduct(@PathVariable("product_id") Long productId,
+                                                @RequestBody ProductDTO productDTO){
+
+        productService.updateProduct(productId, productDTO);
+
+        return ResponseEntity.ok("update product");
+    }
+
+    @DeleteMapping("/admin/product/{product_id}")
+    public ResponseEntity<String> deleteProduct(
+            @PathVariable("product_id") Long productId) throws NotFoundException {
+
+        productService.deleteProduct(productId);
+        return ResponseEntity.ok("delete product");
+    }
+
+
+
+
     /**
      * 전체 상품 조회
      * @return
@@ -40,5 +72,4 @@ public class ProductController {
         Product product = productService.findById(id);
         return ResponseEntity.ok(product);
     }
-
 }
--- src/main/java/kr/casealot/shop/domain/product/entity/Product.java
+++ src/main/java/kr/casealot/shop/domain/product/entity/Product.java
@@ -14,6 +14,7 @@ import javax.persistence.*;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
+@Builder
 @Table(name = "PRODUCT")
 public class Product extends BaseTimeEntity {
     @Id
