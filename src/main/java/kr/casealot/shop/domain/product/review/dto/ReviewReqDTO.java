f3986d76c8afa82bb9b0dcff4a14b72268ef5b65
IamJaehyun <lvet0330@gmail.com>
2023-06-02 17:22:13 +0900

    fix: reviewDTO, Mapping Address

--- src/main/java/kr/casealot/shop/domain/product/review/dto/ReviewReqDTO.java
+++ src/main/java/kr/casealot/shop/domain/product/review/dto/ReviewReqDTO.java
@@ -3,13 +3,15 @@ package kr.casealot.shop.domain.product.review.dto;
 import kr.casealot.shop.global.entity.BaseTimeEntity;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
-@Builder
 @Setter
+@NoArgsConstructor
 public class ReviewReqDTO extends BaseTimeEntity {
     //토큰 통해 작성 + 상품 페이지에 들어가서 작성 (사람과, 상품에 대한 정보 없어도 됨)
+    private Long productSeq; //어떤 상품인지에 대한 정보
     private Double rating; //별점
     private String reviewText; //리뷰 내용
 }
