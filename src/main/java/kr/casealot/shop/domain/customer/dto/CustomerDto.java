c426adb8061a80711b9e9159b44ed0bc129707ca
IamJaehyun <lvet0330@gmail.com>
2023-06-02 16:41:22 +0900

    fix: customer phoneNumber, logindto
    customer 에 휴대폰 번호 추가, loginDto 수정

--- src/main/java/kr/casealot/shop/domain/customer/dto/CustomerDto.java
+++ src/main/java/kr/casealot/shop/domain/customer/dto/CustomerDto.java
@@ -12,6 +12,7 @@ public class CustomerDto {
     private String id;
     private String name;
     private String password;
+    private String phoneNumber;
     private String email;
     private String profileImageUrl;
     private String address;
