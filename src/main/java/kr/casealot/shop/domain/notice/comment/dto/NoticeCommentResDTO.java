81abf5a3641409d42e146e623d5e87e3430a1c83
JaeYooooon <dhy04050@naver.com>
2023-06-04 01:21:52 +0900

    feat : get notice 1. 특정 공지 조회 기능 구현 TODO 조회시 댓글정보에 notice_id 정보 불필요 댓글 수정, 삭제 url 수정 필요

--- src/main/java/kr/casealot/shop/domain/notice/comment/dto/NoticeCommentResDTO.java
+++ src/main/java/kr/casealot/shop/domain/notice/comment/dto/NoticeCommentResDTO.java
@@ -1,15 +1,16 @@
 package kr.casealot.shop.domain.notice.comment.dto;
 
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 @Builder
 public class NoticeCommentResDTO {
     private Long id;
     private String customerId;
+    private Long noticeId;
     private String title;
     private String content;
 }
