e472e9dd64f756f65ee0ab4ac9d0db50bf86aed2
JaeYooooon <dhy04050@naver.com>
2023-06-06 04:14:31 +0900

    fix : merge 1. notice & notice comment 복구

--- src/main/java/kr/casealot/shop/domain/notice/dto/NoticeDetailDTO.java
+++ src/main/java/kr/casealot/shop/domain/notice/dto/NoticeDetailDTO.java
@@ -1,7 +1,6 @@
 package kr.casealot.shop.domain.notice.dto;
 
 import kr.casealot.shop.domain.notice.comment.dto.NoticeCommentResDTO;
-import kr.casealot.shop.domain.notice.comment.entity.NoticeComment;
 import lombok.*;
 
 import java.util.List;
@@ -16,7 +15,6 @@ public class NoticeDetailDTO {
     private String customerId;
     private String title;
     private String content;
-    private String photoUrl;
     private int views;
     private List<NoticeCommentResDTO> noticeCommentList;
 }
