36756d8e2e26c2a7bd30ae5364a6d6a03966ece5
ChangheeKim <kch0348@gmail.com>
2023-06-07 00:16:43 +0900

    fix: product delete fix 1. 상품 삭제 오류 조치

--- src/main/java/kr/casealot/shop/domain/file/repository/UploadFileRepository.java
+++ src/main/java/kr/casealot/shop/domain/file/repository/UploadFileRepository.java
@@ -6,4 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.UUID;
 
 public interface UploadFileRepository extends JpaRepository<UploadFile, UUID> {
+    void deleteByUuid(String uuid);
 }
