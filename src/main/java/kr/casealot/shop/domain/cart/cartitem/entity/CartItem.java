c3c4b9700d21f97ae49999236ea5d0d91087d0c9
IamJaehyun <lvet0330@gmail.com>
2023-06-07 14:10:31 +0900

    feat: cart temp
    카트 임시

---
+++ src/main/java/kr/casealot/shop/domain/cart/cartitem/entity/CartItem.java
@@ -0,0 +1,51 @@
+package kr.casealot.shop.domain.cart.cartitem.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import kr.casealot.shop.domain.cart.entity.Cart;
+import kr.casealot.shop.domain.product.entity.Product;
+import kr.casealot.shop.global.entity.BaseTimeEntity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "CART_ITEM")
+public class CartItem extends BaseTimeEntity {
+    @JsonIgnore
+    @Id
+    @Column(name = "CARTITEM_SEQ")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long seq;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name="CART_ID")
+    private Cart cart;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name="PRODUCT_ID")
+    private Product product;
+
+    private int count; // 상품 개수
+
+    public static CartItem createCartItem(Cart cart, Product product, int amount) {
+        CartItem cartItem = new CartItem();
+        cartItem.setCart(cart);
+        cartItem.setProduct(product);
+        cartItem.setCount(amount);
+        return cartItem;
+    }
+
+    // 이미 담겨있는 물건 또 담을 경우 수량 증가
+    public void addCount(int count) {
+        this.count += count;
+    }
+}
+
---
+++ src/main/java/kr/casealot/shop/domain/cart/repository/CartRepository.java
@@ -0,0 +1,10 @@
+package kr.casealot.shop.domain.cart.repository;
+
+import kr.casealot.shop.domain.cart.entity.Cart;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CartRepository extends JpaRepository<Cart, Long> {
+    Cart findByCustomerId(Long customerId);
+}
