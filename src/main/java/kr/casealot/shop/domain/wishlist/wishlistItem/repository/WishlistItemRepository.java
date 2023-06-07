1bbe93867391a845d6899a4fe5cbd34264bbeb9e
JaeYooooon <dhy04050@naver.com>
2023-06-07 21:27:09 +0900

    feat : delete product to wishlist 1. 위시리스트에 상품 삭제 구현

--- src/main/java/kr/casealot/shop/domain/wishlist/wishlistItem/repository/WishlistItemRepository.java
+++ src/main/java/kr/casealot/shop/domain/wishlist/wishlistItem/repository/WishlistItemRepository.java
@@ -1,9 +1,12 @@
 package kr.casealot.shop.domain.wishlist.wishlistItem.repository;
 
+import kr.casealot.shop.domain.product.entity.Product;
+import kr.casealot.shop.domain.wishlist.entity.Wishlist;
 import kr.casealot.shop.domain.wishlist.wishlistItem.entity.WishlistItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface WishlistItemRepository extends JpaRepository<WishlistItem, Long> {
+    WishlistItem findByProductAndWishlist(Product product, Wishlist wishlist);
 }
