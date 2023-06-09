817f93e0a2468c02db4708a16b0e4b76e11ae159
ChangheeKim <kch0348@gmail.com>
2023-06-10 02:55:21 +0900

    fix: product modify 1. 이미지 업로드 수정 추가 2. product 수정시 기존에 등록된 이미지

---
+++ src/main/java/kr/casealot/shop/domain/product/exception/NotExistProduct.java
@@ -0,0 +1,8 @@
+package kr.casealot.shop.domain.product.exception;
+
+public class NotExistProduct extends Exception{
+
+    public NotExistProduct(String message) {
+        super(message);
+    }
+}
