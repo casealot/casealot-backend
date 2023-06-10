7f224c848fe9ded276bfe4a550cafecaf5f8572e
ChangheeKim <kch0348@gmail.com>
2023-06-10 19:48:54 +0900

    fix: error 테스트

--- src/main/java/kr/casealot/shop/global/exception/DuplicateException.java
+++ src/main/java/kr/casealot/shop/global/exception/DuplicateException.java
@@ -3,6 +3,9 @@ package kr.casealot.shop.global.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+/**
+ * https://www.baeldung.com/spring-response-status
+ */
 @ResponseStatus(code = HttpStatus.CONFLICT)
 public class DuplicateException extends Exception{
     public DuplicateException(String message) {
