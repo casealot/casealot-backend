d8baccde4ac23832a1b156c3dd5277567d6576cb
ChangheeKim <kch0348@gmail.com>
2023-06-10 19:28:22 +0900

    fix: jwt 테스트

--- src/main/java/kr/casealot/shop/domain/customer/service/CustomerService.java
+++ src/main/java/kr/casealot/shop/domain/customer/service/CustomerService.java
@@ -88,10 +88,10 @@ public class CustomerService {
         RoleType roleType = customer.getRoleType();
 
         Date now = new Date();
-
         // 토큰 생성 (jwt)
         // 토큰 유효 기간 설정 (30분 후)
-        long jwtExpiry = now.getTime() + appProperties.getAuth().getTokenExpiry() + (60 * 60 * 24 * 30); //1달로 설정
+        //long jwtExpiry = now.getTime() + appProperties.getAuth().getTokenExpiry() + (60 * 60 * 24 * 30); //1달로 설정
+        long jwtExpiry = now.getTime() + 1000 * 60 * 2; // 테스트 2분
 
         AuthToken authToken = authTokenProvider.createAuthToken(
                 customer.getId(),
