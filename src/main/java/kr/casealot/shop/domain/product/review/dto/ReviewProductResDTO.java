5ae8aff0cd8cab3086e66562f6230a5dd536b1a6
IamJaehyun <lvet0330@gmail.com>
2023-06-13 17:52:40 +0900

    fix: Review & ReviewComment DTO
    리뷰용 DTO 와 상품조회시 필요한 리뷰, 리뷰댓글에 대한 DTO 수정

--- src/main/java/kr/casealot/shop/domain/product/review/dto/ReviewProductResDTO.java
+++ src/main/java/kr/casealot/shop/domain/product/review/dto/ReviewProductResDTO.java
@@ -2,7 +2,7 @@ package kr.casealot.shop.domain.product.review.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import kr.casealot.shop.domain.product.review.reviewcomment.dto.ReviewCommentResDTO;
+import kr.casealot.shop.domain.product.review.reviewcomment.dto.ReviewCommentProductResDTO;
 import lombok.*;
 
 import java.time.LocalDateTime;
@@ -13,13 +13,14 @@ import java.util.List;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-public class ReviewResDTO {
+public class ReviewProductResDTO {
     private Long id;
     private String customerName; //고객 이름
     private Double rating; //별점
     private String reviewText; //리뷰 내용
+    private String available; //수정, 삭제가능여부
     @JsonProperty
-    private List<ReviewCommentResDTO> reviewCommentList;
+    private List<ReviewCommentProductResDTO> reviewCommentList;
 
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
     private LocalDateTime createdDt;
