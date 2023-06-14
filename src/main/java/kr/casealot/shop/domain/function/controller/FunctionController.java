f8faaf95a6f21548b9591c32f8d1a049c70746c1
IamJaehyun <lvet0330@gmail.com>
2023-06-15 00:11:21 +0900

    feat: get todayQna using date
    오늘 날짜 이용해서 오늘의 질문 개수 가져오기 성공

--- src/main/java/kr/casealot/shop/domain/function/controller/FunctionController.java
+++ src/main/java/kr/casealot/shop/domain/function/controller/FunctionController.java
@@ -1,9 +1,16 @@
 package kr.casealot.shop.domain.function.controller;
 
 import io.swagger.annotations.Api;
+import java.time.LocalDateTime;
+import kr.casealot.shop.domain.function.dto.FunctionDTO;
+import kr.casealot.shop.domain.function.service.FunctionService;
+import kr.casealot.shop.global.common.APIResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -13,4 +20,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/cal/v1/function")
 public class FunctionController {
 
+  private final FunctionService functionService;
+
+  @GetMapping("/daily")
+  public APIResponse<FunctionDTO> getDailyOrderData(
+      @RequestParam("date") @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss") LocalDateTime date) {
+    return functionService.getFunction(date);
+  }
 }
--- src/main/java/kr/casealot/shop/domain/function/repository/FunctionRepository.java
+++
@@ -1,9 +0,0 @@
-package kr.casealot.shop.domain.function.repository;
-
-import kr.casealot.shop.domain.function.entity.Function;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface FunctionRepository extends JpaRepository<Function, Long> {
-}
