986abb7eee19ef337d073a23c92018049dd976db
IamJaehyun <lvet0330@gmail.com>
2023-06-15 02:02:27 +0900

    feat: get weekly
    일자별 요약 7일치 구현 완료

---
+++ src/main/java/kr/casealot/shop/domain/function/dto/FunctionWeekDTO.java
@@ -0,0 +1,25 @@
+package kr.casealot.shop.domain.function.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class FunctionWeekDTO {
+
+  @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd", timezone = "Asia/Seoul")
+  private LocalDateTime today;
+  private long todayOrder;
+  private long todayCash;
+  private long todaySignIn;
+  private long todayQna;
+  private long todayReview;
+}
