493efbd8484b5b0f8f3fcc6d848a8b7dd33d454b
JaeYooooon <dhy04050@naver.com>
2023-06-18 02:38:59 +0900

    fix : create order 1. 주문생성시 StringUtil generateOrderNumber 로 주문번호 받아옴

---
+++ src/main/java/kr/casealot/shop/global/exception/OrderCancelException.java
@@ -0,0 +1,15 @@
+package kr.casealot.shop.global.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+/**
+ * https://www.baeldung.com/spring-response-status
+ */
+@ResponseStatus(code = HttpStatus.BAD_REQUEST)
+public class OrderCancelException extends RuntimeException{
+    static final String CANT_CANCEL_ORDER= "주문을 취소할 수 없습니다.";
+    public OrderCancelException() {
+        super(CANT_CANCEL_ORDER);
+    }
+}
