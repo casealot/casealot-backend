6fcfeb664f0dd3e0ae8db0c74a27828cd04ac141
ChangheeKim <kch0348@gmail.com>
2023-06-18 02:15:35 +0900

    feat: order update

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM openjdk:17
+FROM openjdk:17-jdk-slim
 ARG JAR_FILE=build/libs/*.jar
 ENV SPRING_PROFILES_ACTIVE=develop
 
--- src/main/java/kr/casealot/shop/domain/order/controller/OrderController.java
+++ src/main/java/kr/casealot/shop/domain/order/controller/OrderController.java
@@ -1,15 +1,39 @@
 package kr.casealot.shop.domain.order.controller;
 
 import io.swagger.annotations.Api;
+import kr.casealot.shop.global.common.APIResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Objects;
 
 @RestController
 @Slf4j
 @RequiredArgsConstructor
-@Api(tags = {"PRODUCT API"}, description = "상품 조회(검색), 상세 조회")
+@Api(tags = {"ORDER API"}, description = "상품 주문")
 @RequestMapping("/cal/v1/order")
 public class OrderController {
+    /**
+     * 주문 정보 조회
+     */
+
+    /**
+     * 주문
+     */
+    @PostMapping
+    public APIResponse order() {
+        return APIResponse.success("");
+    }
+
+    /**
+     * 주문 취소
+     */
+    @DeleteMapping
+    public APIResponse cancel() {
+        return APIResponse.success("");
+    }
 }
