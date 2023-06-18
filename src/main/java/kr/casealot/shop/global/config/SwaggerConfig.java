75e79794ec12ce1107b32e8c373cc2cb71da11b2
ChangheeKim <kch0348@gmail.com>
2023-06-19 02:03:03 +0900

    fix: Swagger-Config add jwt 1. swagger jwt 인증 추가

--- src/main/java/kr/casealot/shop/global/config/SwaggerConfig.java
+++ src/main/java/kr/casealot/shop/global/config/SwaggerConfig.java
@@ -1,5 +1,6 @@
 package kr.casealot.shop.global.config;
 
+import com.nimbusds.jwt.JWT;
 import io.swagger.annotations.Api;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,11 +11,16 @@ import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 @Configuration
@@ -27,20 +33,44 @@ public class SwaggerConfig {
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
+                .useDefaultResponseMessages(false)
                 .select()
                 .apis(RequestHandlerSelectors.withClassAnnotation(Api.class)) // API 컨트롤러가 있는 패키지 지정
                 .paths(PathSelectors.any())
                 .build()
                 .apiInfo(apiInfo())
+                .securityContexts(Arrays.asList(securityContext()))
+                .securitySchemes(Arrays.asList(apiKey()))
                 .produces(DEFAULT_PRODUCES_AND_CONSUMES)
                 .consumes(DEFAULT_PRODUCES_AND_CONSUMES);
     }
 
     private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
-                .title("CASEALOT SHOP API Document")
+                .title("CASEALOT-SHOP REST API")
                 .description("Swagger Documentation for REST API")
                 .version("1.0.0")
                 .build();
+    }
+
+    private SecurityContext securityContext() {
+        return springfox
+                .documentation
+                .spi.service
+                .contexts
+                .SecurityContext
+                .builder()
+                .securityReferences(defaultAuth()).forPaths(PathSelectors.any()).build();
+    }
+
+    List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return Arrays.asList(new SecurityReference("JWT", authorizationScopes));
+    }
+
+    private ApiKey apiKey() {
+        return new ApiKey("JWT", "Authorization", "header");
     }
 }
