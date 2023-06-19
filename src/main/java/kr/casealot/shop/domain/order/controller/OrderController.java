c4cb8f39bf7a3fb7398eb366d7616c17bf526ec1
JaeYooooon <dhy04050@naver.com>
2023-06-19 12:54:25 +0900

    fix : order controller 1. swagger 적용

--- src/main/java/kr/casealot/shop/domain/order/controller/OrderController.java
+++ src/main/java/kr/casealot/shop/domain/order/controller/OrderController.java
@@ -1,14 +1,13 @@
 package kr.casealot.shop.domain.order.controller;
 
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import kr.casealot.shop.domain.order.dto.OrderDTO;
-import kr.casealot.shop.domain.order.entity.Order;
-import kr.casealot.shop.domain.order.repository.OrderRepository;
 import kr.casealot.shop.domain.order.service.OrderService;
 import kr.casealot.shop.global.common.APIResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
@@ -23,27 +22,32 @@ public class OrderController {
     private final OrderService orderService;
 
     @PostMapping
-    public APIResponse<OrderDTO.Response> createOrder(@RequestBody OrderDTO.Request request,
-                                                      Principal principal){
+    @ApiOperation(value = "주문 생성", notes = "주문을 생성한다.")
+    public APIResponse<OrderDTO.Response> createOrder(
+            @ApiParam(value = "주문 생성 요청 DTO - 아이템 목록") @RequestBody OrderDTO.createOrder createOrder,
+                                                      Principal principal) {
 
 
-        return orderService.createOrder(request, principal);
+        return orderService.createOrder(createOrder, principal);
     }
 
     @PostMapping("/{orderId}/cancel")
-    public APIResponse<Void> cancelOrder(@PathVariable Long orderId, Principal principal){
+    @ApiOperation(value = "주문 취소", notes = "주문 취소 - 주문 상태를 취소로 변경")
+    public APIResponse<Void> cancelOrder(@ApiParam(value = "주문 취소 요청 DTO - 주문 ID") @PathVariable Long orderId, Principal principal) {
 
         return orderService.cancelOrder(orderId, principal);
     }
 
     @GetMapping("/{orderId}")
-    public APIResponse<OrderDTO.Response> getOrderDetail(@PathVariable Long orderId,
+    @ApiOperation(value = "주문 상세 조회", notes = "본인의 주문 내역 상세조회")
+    public APIResponse<OrderDTO.Response> getOrderDetail(@ApiParam(value = "주문 상세 조회 DTO - 주문 ID") @PathVariable Long orderId,
                                                          Principal principal) {
 
         return orderService.getOrderDetail(orderId, principal);
     }
 
     @GetMapping
+    @ApiOperation(value = "주문 목록 조회", notes = "본인의 주문 목록 조회")
     public APIResponse<List<OrderDTO.Response>> getOrderList(Principal principal) {
 
         return orderService.getOrderList(principal);
