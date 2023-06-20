2c6aebc28736fe513ef84c1cce51b9cf7f231ebb
IamJaehyun <lvet0330@gmail.com>
2023-06-20 21:34:21 +0900

    fix: ApiParam & ApiOperation Notice comment
    add swagger ApiParam & ApiOperation Notice comment

--- src/main/java/kr/casealot/shop/domain/notice/comment/controller/NoticeCommentController.java
+++ src/main/java/kr/casealot/shop/domain/notice/comment/controller/NoticeCommentController.java
@@ -1,6 +1,8 @@
 package kr.casealot.shop.domain.notice.comment.controller;
 
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import kr.casealot.shop.domain.notice.comment.dto.NoticeCommentReqDTO;
 import kr.casealot.shop.domain.notice.comment.dto.NoticeCommentResDTO;
 import kr.casealot.shop.domain.notice.comment.service.NoticeCommentService;
@@ -16,35 +18,40 @@ import java.security.Principal;
 @Api(tags = {"NOTICE COMMENT API"}, description = "NOTICE COMMENT 관련 API")
 @RequestMapping("/cal/v1/notice")
 public class NoticeCommentController {
-    private final NoticeCommentService noticeCommentService;
 
-    @PostMapping("/{notice_id}")
-    public APIResponse<NoticeCommentResDTO> createComment(
-            @PathVariable("notice_id") Long noticeId,
-            @RequestBody NoticeCommentReqDTO noticeCommentReqDTO,
-            HttpServletRequest request,
-            Principal principal){
+  private final NoticeCommentService noticeCommentService;
 
-        return noticeCommentService.createComment(noticeId, noticeCommentReqDTO, request, principal);
-    }
+  @PostMapping("/{notice_id}")
+  @ApiOperation(value = "공지 댓글 생성", notes = "공지 댓글을 등록한다.")
+  public APIResponse<NoticeCommentResDTO> createComment(
+      @ApiParam(value = "공지 ID") @PathVariable("notice_id") Long noticeId,
+      @ApiParam(value = "공지 댓글 DTO") @RequestBody NoticeCommentReqDTO noticeCommentReqDTO,
+      HttpServletRequest request,
+      Principal principal) {
 
-    // 댓글 삭제
-    @DeleteMapping("/{comment_id}")
-    public APIResponse<NoticeCommentResDTO> deleteComment(
-            @PathVariable("comment_id") Long commentId,
-            HttpServletRequest request,
-            Principal principal
-    ){
-        return noticeCommentService.deleteComment(commentId, request, principal);
-    }
-    // 댓글 수정
-    @PutMapping("/{comment_id}")
-    public APIResponse<NoticeCommentResDTO> updateComment(
-            @PathVariable("comment_id") Long commentId,
-            @RequestBody NoticeCommentReqDTO noticeCommentReqDTO,
-            HttpServletRequest request,
-            Principal principal){
+    return noticeCommentService.createComment(noticeId, noticeCommentReqDTO, request, principal);
+  }
 
-        return noticeCommentService.updateComment(commentId, noticeCommentReqDTO, request, principal);
-    }
+  // 댓글 삭제
+  @DeleteMapping("/{comment_id}")
+  @ApiOperation(value = "공지 댓글 삭제", notes = "자신이 작성한 공지 댓글을 삭제한다.")
+  public APIResponse<NoticeCommentResDTO> deleteComment(
+      @ApiParam(value = "공지 댓글 ID") @PathVariable("comment_id") Long commentId,
+      HttpServletRequest request,
+      Principal principal
+  ) {
+    return noticeCommentService.deleteComment(commentId, request, principal);
+  }
+
+  // 댓글 수정
+  @PutMapping("/{comment_id}")
+  @ApiOperation(value = "공지 댓글 수정", notes = "자신이 작성한 공지 댓글을 수정한다.")
+  public APIResponse<NoticeCommentResDTO> updateComment(
+      @ApiParam(value = "공지 댓글 ID") @PathVariable("comment_id") Long commentId,
+      @ApiParam(value = "공지 댓글 수정 DTO") @RequestBody NoticeCommentReqDTO noticeCommentReqDTO,
+      HttpServletRequest request,
+      Principal principal) {
+
+    return noticeCommentService.updateComment(commentId, noticeCommentReqDTO, request, principal);
+  }
 }
