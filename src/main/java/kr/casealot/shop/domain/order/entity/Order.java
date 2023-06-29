759381073ad2026a3b8b67ad0d83b4d23e90ef34
IamJaehyun <lvet0330@gmail.com>
2023-06-29 16:43:20 +0900

    feat: delivery status
    배송 상태 enum 타입 지정
    주문시 운송장번호, 배송상태(배송 준비중) 넣음
    주문 취소시 운송장번호, 배송상태(주문 취소) 수정

--- src/main/java/kr/casealot/shop/domain/order/entity/Order.java
+++ src/main/java/kr/casealot/shop/domain/order/entity/Order.java
@@ -1,19 +1,31 @@
 package kr.casealot.shop.domain.order.entity;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
 import kr.casealot.shop.domain.customer.entity.Customer;
+import kr.casealot.shop.domain.order.delivery.dto.DeliveryStatus;
 import kr.casealot.shop.domain.order.dto.OrderStatus;
 import kr.casealot.shop.domain.payment.entity.Payment;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
-import javax.persistence.*;
-import java.math.BigDecimal;
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
 
 @Getter
 @Setter
@@ -31,11 +43,20 @@ public class Order{
     @Column(name = "order_dt")
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
     private LocalDateTime orderDt;
+
     @Column(name = "order_status")
     @Enumerated(EnumType.STRING)
     private OrderStatus orderStatus;
+
     @Column(name = "total_amount")
     private int totalAmount;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "delivery_status")
+    private DeliveryStatus deliveryStatus;
+
+    @Column(name = "delivery_number")
+    private String deliveryNumber;
     @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "customer_id")
     private Customer customer;
@@ -46,6 +67,7 @@ public class Order{
     @OneToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "payment_id")
     private Payment payment;
+
 
 
     public void addOrderProduct(OrderProduct orderProduct) {
